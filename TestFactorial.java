1d2b42a9e5bfc3af93a8b1b9d5ef6d6f15cd16e9
tejaswini-939 <visthargallatejaswini@gmail.com>
2026-08-12 10:43:41 +0530

    Added week 5

---
+++ TestFactorial.java
@@ -0,0 +1,13 @@
+public class TestFactorial {
+
+    public static void main(String[] args) {
+
+        int result = Factorial.calculate(5);
+
+        if (result == 120) {
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+        }
+    }
+}
